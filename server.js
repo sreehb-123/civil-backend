191a78f54c2d47b8f7c30a9f08e06626df7ab6d4
sreehb-123 <sreeh190206@gmail.com>
2025-01-24 00:59:25 +0530

    minorr

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+// }));
+
+app.use(cors());
 
 app.use(express.json());
 
